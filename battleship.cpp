cf0512ba35aa0856adcd4067ec4572e256909460
JeremiahK96 <jrknol96@gmail.com>
2021-09-09 10:59:02 -0500

    Add game loop with prompt for playing again

--- battleship.cpp
+++ battleship.cpp
@@ -1,9 +1,33 @@
+#include <iostream>
+#include <string>
 #include <cassert>
+
 #include "SplashScreen.hpp"
+
+bool PlayGame ()
+{
+    std::string input;
+    assert(input.empty());
+
+    while (input.compare("y") && input.compare("n"))
+    {
+        std::cout << "Would you like to play again? y/n   ";
+        std::getline(std::cin, input);
+    }
+    assert(!input.compare("y") || !input.compare("n"));
+
+    if (!input.compare("y"))
+        return true;
+    return false;
+}
 
 int main (int argv, char **argc)
 {
     SplashScreen();
+
+    // play games until user chooses to quit
+    while (PlayGame())
+    {}
 
     return 0;
 }
