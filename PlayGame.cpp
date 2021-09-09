2985d71ad0d0aeca9a02bfa25d11130ebfde1419
JeremiahK96 <jrknol96@gmail.com>
2021-09-09 14:19:15 -0500

    Move PlayGame to separate file

---
+++ PlayGame.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <cassert>
+
+bool PlayGame ()
+{
+    std::string input;
+    assert(input.empty());
+
+    while (input.compare("y") && input.compare("n"))
+    {
+        assert(input.compare("y") && input.compare("n"));
+        std::cout << "Would you like to play again? y/n   ";
+        std::getline(std::cin, input);
+    }
+    assert(!input.compare("y") || !input.compare("n"));
+
+    if (!input.compare("y"))
+        return true;
+    return false;
+}
--- battleship.cpp
+++ battleship.cpp
@@ -1,31 +1,11 @@
-#include <iostream>
-#include <string>
-#include <cassert>
-
 #include "SplashScreen.hpp"
-
-bool PlayGame ()
-{
-    std::string input;
-    assert(input.empty());
-
-    while (input.compare("y") && input.compare("n"))
-    {
-        std::cout << "Would you like to play again? y/n   ";
-        std::getline(std::cin, input);
-    }
-    assert(!input.compare("y") || !input.compare("n"));
-
-    if (!input.compare("y"))
-        return true;
-    return false;
-}
+#include "PlayGame.hpp"
 
 int main (int argv, char **argc)
 {
     SplashScreen();
 
-    // play games until user chooses to quit
+    // keep playing games until user chooses to quit
     while (PlayGame())
     {}
 
