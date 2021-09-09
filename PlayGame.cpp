9ec6cc53b691f68261b5f0a80db12fa60317f801
JeremiahK96 <jrknol96@gmail.com>
2021-09-09 15:59:23 -0500

    Add QueryNumPlayers function

--- PlayGame.cpp
+++ PlayGame.cpp
@@ -6,7 +6,28 @@
 
 bool PlayGame ()
 {
+    int players = QueryNumPlayers();
+
     return QueryNewGame();
+}
+
+int QueryNumPlayers ()
+{
+    int players = -1;
+
+    while (players < 0 || players > 2)
+    {
+        std::string input;
+
+        std::cout << "Number of players   ";
+        std::getline(std::cin, input);
+
+        if (input.size() && std::isdigit(input.at(0)))
+            players = std::stoi(input);
+    }
+    assert(players >= 0 && players <= 2);
+
+    return players;
 }
 
 bool QueryNewGame ()
@@ -16,8 +37,7 @@ bool QueryNewGame ()
 
     while (input.compare("y") && input.compare("n"))
     {
-        assert(input.compare("y") && input.compare("n"));
-        std::cout << "Would you like to play again? y/n   ";
+        std::cout << "Would you like to play again?   ";
         std::getline(std::cin, input);
     }
     assert(!input.compare("y") || !input.compare("n"));
--- PlayGame.hpp
+++ PlayGame.hpp
@@ -1,4 +1,5 @@
 #pragma once
 
 bool PlayGame ();
+int QueryNumPlayers ();
 bool QueryNewGame ();
